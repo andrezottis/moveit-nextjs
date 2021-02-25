fc94bc28a077b578fe918a2d9879d137fc3f378d
André Zottis <zottis.andre@outlook.com>
2021-02-25 13:24:46 -0300

    Added notification

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,5 +1,5 @@
 import { cwd } from "process";
-import { createContext, useState, ReactNode } from "react";
+import { createContext, useState, ReactNode, useEffect } from "react";
 import challenges from "../../challenges.json";
 
 interface Challenge {
@@ -35,6 +35,10 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
   const experienceToNextLevel = Math.pow((level +1) * 4, 2);
 
+  useEffect(()=> {
+    Notification.requestPermission();
+  }, [])
+
   function levelUp() {
     setLevel(level + 1);
   }
@@ -44,6 +48,12 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const challenge = challenges[randomChallengeIndex];
 
     setActiveChallenge(challenge);
+
+    if(Notification.permission === 'granted'){
+      new Notification('Novo desafio', {
+        body: `Vaalendo ${challenge.amount}xp!`
+      })
+    }
   }
 
   function resetChallenge(){
